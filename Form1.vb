d7f39998f53a9aac39fae5cd7f9311b566293f2e
Jochim Liveyns <jochimliveyns@cmac.com>
2016-06-30 13:24:23 +0200

    Initial Commit

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim paper As Graphics
+        Dim foto As New Bitmap("Koereurchinees.jpg")
+
+        paper = PictureBox1.CreateGraphics()
+        Dim myPen As Pen = New Pen(Color.Black)
+        paper.DrawImage(foto, 0, 0, 260, 209)
+
+    End Sub
+End Class
